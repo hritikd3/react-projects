fa5c8330a6df677a73c3cfb95d06817a08ab9c32
hritikdangi3 <hritikdangi3@gmail.com>
2022-02-07 16:41:50 +0530

    updated files

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,18 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [friends, setFriends] = useState(data);
+  return (
+    <main>
+      <section className="container">
+        <h3>{friends.length} birthdays today</h3>
+        <List friends={friends} />
+        <button onClick={() => setFriends([])} >clear all</button>
+        {/* console.log("click is wrking") */}
+      </section>
+    </main>
+  );
 }
 
 export default App;
