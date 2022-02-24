8f8f6e1475f17be9832753c9c21ef3d3a83bdfba
hritikdangi3 <hritikdangi3@gmail.com>
2022-02-24 18:42:25 +0530

    Cocktailist & searchForm updated

--- 15-cocktails/setup/src/components/CocktailList.js
+++ 15-cocktails/setup/src/components/CocktailList.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import Cocktail from './Cocktail'
 import Loading from './Loading'
 import { useGlobalContext } from '../context'
 
 const CocktailList = () => {
+  const{cocktails, loading}=useGlobalContext();
+
+  if(loading){
+    return <Loading />
+  }
+  if(cocktails.length<1){
+    return (
+      <h2> No cocktails matched your search</h2>
+    )
+  }
   return (
     <div>
-      <h2>cocktail list component</h2>
+      <h2 className='section-title'>cocktail list component</h2>
     </div>
   )
 }
